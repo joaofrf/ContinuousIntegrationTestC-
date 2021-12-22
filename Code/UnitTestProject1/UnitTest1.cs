0ccd3b30a6f338a9433457615ac8974cb082460f
joao.ferreira@bitcliq.com <joao.ferreira@bitcliq.com>
2021-12-22 10:33:25 +0000

    Added unit test project

---
+++ Code/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebApplication1.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddMethodTest()
+        {
+            int x = 10, y = 5;
+            Assert.IsTrue(Helper.Add(x, y) == 15);
+            y = 10;
+            Assert.IsTrue(Helper.Add(x, y) == 20);
+
+            Assert.AreEqual(Helper.Add(x, y), Helper.Add(x, y));
+        }
+    }
+}
